1c541d59f0ff4a45d13b8b9e42940bd3cd42e1e0
beongchang0423 <43203949+beongchang0423@users.noreply.github.com>
2018-09-28 02:36:03 +0900

    Update 인자.py

--- 4.7-4.12/인자.py
+++ 4.7-4.12/인자.py
@@ -1,1 +1,15 @@
+#위치인자
 def menu(wine, entree, dessert):
+  return {'wine': wine, 'entree': entree, 'dessert' : dessert}
+menu('chardonnay','chicken','cake')
+menu('beef','bagel','bordeaux')
+#키워드인자
+menu(entree='beef',dessert='bagel',wine = 'bordeaux')
+menu('frontenac',dessert ='flan', entree= 'fish')
+#기본 매개변수값 지정하기
+def menu_2(wine,entree,dessert='pudding'):
+  return {'wine': wine, 'entree': entree, 'dessert' : dessert}
+
+menu_2('chardonnay','chicken')
+
+menu_2('dunkelfelder','duck','doughnut')
