70e56398e5dbb82d7f0056ca87de79bbed77f2f3
beongchang0423 <43203949+beongchang0423@users.noreply.github.com>
2018-09-28 12:26:43 +0900

    Update 클로저 람다.py

--- 4.7-4.12/클로저 람다.py
+++ 4.7-4.12/클로저 람다.py
@@ -1,1 +1,17 @@
-def
+#클로져
+#익명함수 : lambda()
+def edit_story(words,func):
+  for word in words:
+    print(func(word))
+stairs = ['thub','meo','hiss']
+def enliven(word):
+  return word. capitalize() + '!'
+
+edit_story(stairs, enliven)
+edit_story_2(stairs, lambda word: word.capitalize() + '!')
+
+sum = lambda a, b: a+b
+sum(3,4)
+def sum(a,b):
+    return a+b
+sum(3,4)
